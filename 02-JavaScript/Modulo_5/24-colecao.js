b89e341747bcf35617fec0d40e6875df29502716
Vinicius Oliveira <vinicius.dsro@hotmail.com>
2023-09-05 21:51:23 -0300

    Atividade 24 do modulo 5 de JavaScript

---
+++ 02-JavaScript/Modulo_5/24-colecao.js
@@ -0,0 +1,16 @@
+// Criar um objeto para armazenar as informações dos participantes e pontuações
+const pontuacoes = {
+    joao: 50,
+    pedro: 120,
+    luiza: 70,
+    marcela: 10,
+    yuri: 34,
+  }
+  
+  // Acessar as informações dos participantes
+  console.log(`Pontuação do João: ${pontuacoes.joao}`)
+  console.log(`Pontuação do Pedro: ${pontuacoes.pedro}`)
+  console.log(`Pontuação da Luiza: ${pontuacoes.luiza}`)
+  console.log(`Pontuação da Marcela: ${pontuacoes.marcela}`)
+  console.log(`Pontuação do Yuri: ${pontuacoes.yuri}`)
+  
